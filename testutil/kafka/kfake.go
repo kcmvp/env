849771cc624216587984645f50f3fc23fc6d08cf
kcmvp <kcheng.mvp@gmail.com>
2026-03-18 21:21:42 +0800

    add lifecycle support

--- testutil/kafka/kfake.go
+++ testutil/kafka/kfake.go
@@ -2,19 +2,54 @@ package kafka
 
 import (
 	"log/slog"
+	"sync"
 
 	"github.com/twmb/franz-go/pkg/kfake"
 )
 
-var _cluster *kfake.Cluster
+var (
+	startOnce sync.Once
+	closeOnce sync.Once
+	_cluster  *kfake.Cluster
+	startErr  error
+)
 
-func init() {
-	var err error
-	opts := []kfake.Opt{kfake.NumBrokers(2),
-		kfake.Ports(57351, 57352)}
-	_cluster, err = kfake.NewCluster(opts...)
-	if err != nil {
-		panic(err)
+// Start starts a mock Kafka cluster. It's safe to call multiple times; subsequent
+// calls will be no-ops. This Start is parameterless and will start three brokers
+// listening on ports 29092,29093,29094 and enable auto topic creation.
+func Start() error {
+	startOnce.Do(func() {
+		// Start 3 brokers on fixed ports and allow auto topic creation.
+		_cluster, startErr = kfake.NewCluster(
+			kfake.Ports(29092, 29093, 29094),
+			kfake.AllowAutoTopicCreation(),
+		)
+		if startErr == nil {
+			slog.Info("Mock Kafka cluster started", "brokers", _cluster.ListenAddrs())
+		}
+	})
+	return startErr
+}
+
+// Close stops the mock Kafka cluster. It's safe to call multiple times.
+// Close stops the mock Kafka cluster. It's safe to call multiple times.
+// Close is idempotent (will only run once).
+func Close() {
+	closeOnce.Do(func() {
+		if _cluster == nil {
+			return
+		}
+		_cluster.Close()
+		_cluster = nil
+		slog.Info("Mock Kafka cluster stopped")
+	})
+}
+
+// Brokers returns the listen addresses of the running mock cluster. If the
+// cluster is not started it returns an empty slice.
+func Brokers() []string {
+	if _cluster == nil {
+		return nil
 	}
-	slog.Info("Mock Kafka cluster started", "brokers", _cluster.ListenAddrs())
+	return _cluster.ListenAddrs()
 }
